7eb8f49051d1cee2bca17a5460b24929ab25bb94
Oskar Owca <71120011+Kriperos@users.noreply.github.com>
2021-10-29 11:56:24 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,28 @@
+
+"""
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+
+"""
+
+"""
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+"""
+
+
+"""
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if "a" in x]
+
+print(newlist)
+"""
+
+
+newlist = [x for x in range(10)]
+
+print(newlist)
